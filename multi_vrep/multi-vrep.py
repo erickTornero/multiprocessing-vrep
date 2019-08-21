3a92745fb65fe9da1902c30f08c9a42312a06b4c
erickTornero <erickdeivy01@gmail.com>
2019-08-20 23:27:15 -0500

    wrapper added

---
+++ multi_vrep/multi-vrep.py
@@ -0,0 +1,8 @@
+from wrapper.wrapper_vrep import VREPQuad
+
+print('successfully imported VREP Wrapper Quadrotor!!')
+
+"""
+
+"""
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name        =   'multiprocess-vrep',
+    version     =   '0.1',
+    description =   'Execute safe multiprocessing VREP',
+    author      =   'Erick Tornero',
+    author_email=   'erickdeivy01@gmail.com',
+    packages    =   ['wrapper']
+)
